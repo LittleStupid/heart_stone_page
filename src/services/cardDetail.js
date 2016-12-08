f7990f87c6e2e315d011bbc4c4109be6cd3e284e
LittleStupid <lttlstpd@live.com>
2016-12-08 14:48:58 +0800

    fix services

--- src/services/cardDetail.js
+++ src/services/cardDetail.js
@@ -2,8 +2,10 @@ import request from '../utils/request';
 import qs from 'qs';
 
 export async function getRemoteCardInfo(params) {
-  return request('https://omgvamp-hearthstone-v1.p.mashape.com/cards/EX1_116?mashape-key=97GnbLpzEemsh6rU25FWlsL2YAvkp1V1IczjsnQO80HhkRdfEf',
+  return request('https://omgvamp-hearthstone-v1.p.mashape.com/cards/EX1_116',
   {
     method: 'get',
+    headers: {"X-Mashape-Key": "97GnbLpzEemsh6rU25FWlsL2YAvkp1V1IczjsnQO80HhkRdfEf",
+             "Accept": "application/json"}
   });
 }
