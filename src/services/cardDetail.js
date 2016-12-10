63471b7118a5e67c1261090b326c538e0e5a727c
LittleStupid <lttlstpd@live.com>
2016-12-10 18:49:34 +0800

    use local api

--- src/services/cardDetail.js
+++ src/services/cardDetail.js
@@ -10,15 +10,19 @@ export async function getRemoteCardInfo() {
 }
 
 export async function getRemoteCardBack() {
-  return request('https://omgvamp-hearthstone-v1.p.mashape.com/cardbacks', {
-    method: 'get',
-    headers: {"X-Mashape-Key": "97GnbLpzEemsh6rU25FWlsL2YAvkp1V1IczjsnQO80HhkRdfEf",
-             "Accept": "application/json"}
-    });
+   return request('http://localhost:3000/api/cardbacks', {
+    //  method: 'get',
+    //  headers: {"Access-Control-Allow-Origin": "*"}
+   });
+  // return request('https://omgvamp-hearthstone-v1.p.mashape.com/cardbacks?limit=3', {
+  //   method: 'get',
+  //   headers: {"X-Mashape-Key": "97GnbLpzEemsh6rU25FWlsL2YAvkp1V1IczjsnQO80HhkRdfEf",
+  //            "Accept": "application/json"}
+  //   });
 }
 
 export async function getRemoteHero() {
-  return request('https://omgvamp-hearthstone-v1.p.mashape.com/cards/types/Hero?collectible=1&rarity=free', {
+  return request('https://omgvamp-hearthstone-v1.p.mashape.com/cards/types/Hero?collectible=1&rarity=free&name=3', {
     method: 'get',
     headers: {"X-Mashape-Key": "97GnbLpzEemsh6rU25FWlsL2YAvkp1V1IczjsnQO80HhkRdfEf",
              "Accept": "application/json"}
